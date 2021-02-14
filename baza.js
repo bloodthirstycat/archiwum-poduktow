01294e3b48bb23c611f61fb5915a16be26a97ff4
example <example@example.org>
2021-02-14 21:47:54 +0100

    baza.js

--- baza.js
+++ baza.js
@@ -1,1 +1,20 @@
-alert("XD");
+var lista = [
+	{
+		id: 333,
+		nazwa: 'test',
+		obrazek: '',
+		opis: '',
+		ean: '',
+	}
+]
+
+function listujNazwy(elem){
+	lista.forEach(function(x){
+		var li_elem = document.createElement("li");
+		var a_elem = document.createElement("a");
+		a_elem.href = '/produkt.html?id=' + x.id;
+		a_elem.innerText = x.nazwa;
+		li_elem.appendChild(a_elem);
+		elem.appendChild(li_elem);
+	});
+}
