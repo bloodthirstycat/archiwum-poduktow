04793be5684cb3c8eeba362bcb9131ca1fa0ad19
example <example@example.org>
2021-02-14 21:55:50 +0100

    baza.js

--- baza.js
+++ baza.js
@@ -12,7 +12,7 @@ function listujNazwy(elem){
 	lista.forEach(function(x){
 		var li_elem = document.createElement("li");
 		var a_elem = document.createElement("a");
-		a_elem.href = '/produkt.html?id=' + x.id;
+		a_elem.href = '/archiwum-poduktow/produkt.html?id=' + x.id;
 		a_elem.innerText = x.nazwa;
 		li_elem.appendChild(a_elem);
 		elem.appendChild(li_elem);
