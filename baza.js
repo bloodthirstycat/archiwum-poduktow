b09867154b276238308522528536728b50d46598
example <example@example.org>
2021-02-15 02:54:28 +0100

    Ma³a zmiana

--- baza.js
+++ baza.js
@@ -5,7 +5,7 @@ var lista = [
 		obrazek: 'Kabanostrrre-70112-big.jpg',
 		opis: 'Chipsy o smaku podwędzanego i dobrze przyprawionego kabanosa.',
 		sklad: 'Brak danych',
-		ean: 'Brak danych',
+		ean: 'Brak danych'
 	},
 	{
 		id: 2,
@@ -13,8 +13,16 @@ var lista = [
 		obrazek: '76976897.jpg',
 		opis: 'Baton ze świeżo prażonymi orzeszkami ziemnymi i chrupkami ryżowymi w karmelu oblanymi mleczną czekoladą.',
 		sklad: 'cukier, orzeszki ziemne, mąka ryżowa, cukier inwertowany, tłuszzc roślinny, odtłuszczone mleko w proszku, tłuszcz kakaowy, miazga kakaowa, serwatka w proszku, syrop glukozowy, tłuszcz mleczny, substancja utrzymująca wilgotność (glicerol), laktoza, jęczmienny ekstrakt słodowy, sól, emulgatory (lecytyna sojowa, E471), cukier gronowy, aromat.',
-		ean: '5000159314732',		
-	}
+		ean: '5000159314732'		
+	},
+	{
+		id: 1,
+		nazwa: 'Nestle Aero czekolada mleczna bąbelkowa',
+		obrazek: '70522167.jpg	',
+		opis: 'Aero to aksamitny mleczny baton z tysiącami czekoladowych bąbelków rozpływających się w ustach. Idealna przekąska kiedy tylko masz na nią ochotę! Baton mleczny Aero to niespotykana przyjemność, jaką zapewnia delikatna, rozpływająca się w ustach czekolada w połączeniu z ciekawą strukturą wnętrza, wypełnioną tysiącami pęcherzyków powietrza. Zapewnia prawdziwą przyjemność jedzenia i zarazem niezapomnianą bąbelkową przygodę. Nestlé Aero: - oryginalna napowietrzana czekolada mleczna - przyjemne doznanie lekkości płynące ze smaku prawdziwej mlecznej czekolady i roztapiających się bąbelków - ekspert od napowietrzanej czekolady od 1935 roku - unikalna receptura wysokiej jakości - wyłącznie naturalne składniki - kakao pochodzące z odpowiedzialnych upraw od zaufanych dostawców',
+		sklad: 'cukier, tłuszcz kakaowy, śmietanka w proszku, miazga kakaowa, odtłuszczone mleko w proszku, emulgatory: lecytyna sojowa, E 476, sól, aromaty, śladowe ilości orzechów ziemnych i laskowych oraz glutenu, masa kakaowa min. 33,8%',
+		ean: 'Brak danych'
+	}	
 ]
 
 function listujNazwy(elem){
