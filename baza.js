806aa7c8f969b8dc1df3473169b134207459304f
robercik101 <robert@osboxes>
2021-09-30 19:13:18 +0000

    ZMIANA 053591078731509244940484896299

--- baza.js
+++ baza.js
@@ -254,6 +254,14 @@ var lista = [
 		opis: 'Galaretka o smaku Moijto',
 		sklad: 'aromaty, błękit brylantowy FCF (E 133), cukier. koncentrat krokosza, kwas cytrynowy (E 330). żelatyna wieprzowa (E 441)',
 		ean: 'Brak informacji'
+	},
+	{
+		id: 34,
+		nazwa: 'Batonik MilkyWay Minute czekoladowy -',
+		obrazek: 'mwminute.jpg',
+		opis: 'Ciasteczko z kremem mlecznym, truskawkami i mleczną czekoladą.',
+		sklad: 'Brak informacji',
+		ean: 'Brak informacji'
 	}
 ]
 
