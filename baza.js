a1bcb173985f145810518990953627265b8fe0d2
example <example@example.org>
2021-12-08 23:40:57 +0100

    E3

--- baza.js
+++ baza.js
@@ -372,16 +372,22 @@ function listujNazwyA(){
 		if(Array.isArray(x.kategoria)){
 			x.kategoria.forEach(function(z){
 				var katEl = document.getElementById('kat' + z);
+				var li_elem = document.createElement("li");
+				var a_elem = document.createElement("a");
+				a_elem.href = '/archiwum-poduktow/produkt.html?id=' + x.id;
+				a_elem.innerText = x.nazwa;
+				li_elem.appendChild(a_elem);
+				katEl.appendChild(li_elem);
 			})
 		}else{
 		var katEl = document.getElementById('kat' + x.kategoria);
-		}
 		var li_elem = document.createElement("li");
 		var a_elem = document.createElement("a");
 		a_elem.href = '/archiwum-poduktow/produkt.html?id=' + x.id;
 		a_elem.innerText = x.nazwa;
 		li_elem.appendChild(a_elem);
 		katEl.appendChild(li_elem);
+		}
 	});
 }
 
